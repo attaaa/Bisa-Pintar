4b8c7afb97cec30e1ef2376f39de5bc26a36ddfc
Hatta <hatta.eka.p@gmail.com>
2018-11-15 18:28:25 +0700

    fungsi daftar

---
+++ functions/signup.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+
+    if (isset($_GET['daftar'])){
+        $username = $_GET['uname'];
+        $password = $_GET['pass'];
+        $name = $_GET['nama'];
+        $email = $_GET['email'];    
+
+        if ($username == 'test') {
+            $_SESSION['daftar'] = 'invalid';
+            header('Location: ../');
+        } else {
+            $_SESSION['daftar'] = 'valid';
+            //go to profil
+        }
+    }
+
+?>
