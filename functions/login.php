a763d85bb8448f4c747f95d5a81e91b663608932
Attaaa <hatta.eka.p@gmail.com>
2018-11-27 13:19:03 +0700

    test

--- functions/login.php
+++ functions/login.php
@@ -4,8 +4,12 @@
     if (isset($_GET['masuk'])){
         $username = $_GET['username'];
         $password = $_GET['password'];
+        $email = 'test@gmail.com';
+        $name = 'test';
         if (($username == 'test') && ($password == 'test')) {
             $_SESSION['user'] = $username;
+            $user = array($username, $password, $name, $email);
+            $_SESSION['user'] = $user;
             $_SESSION['login'] = 'valid';
             header('Location: ../profile.php');
         } else {
