908b19bf27bde23702ae5d0fc246d793e78e4cb8
Attaaa <hatta.eka.p@gmail.com>
2018-11-27 08:50:31 +0700

    small changes

--- materi/mpspec.php
+++ materi/mpspec.php
@@ -2,8 +2,7 @@
     session_start(); 
     if (!isset($_SESSION['login'])){
         header('Location: ../index.php');
-    }    
-    $_SESSION['location'] = 'Location: ../index.php';
+    }
 ?>
 
 <!DOCTYPE html>
